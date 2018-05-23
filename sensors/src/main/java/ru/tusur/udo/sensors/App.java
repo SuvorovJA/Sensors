ed313e72fc2efd653f2bb0bca239038f7685929b
u201 <u201@NK201-14.tusur.local>
2018-05-23 21:23:39 +0700

    v180523

--- sensors/src/main/java/ru/tusur/udo/sensors/App.java
+++ sensors/src/main/java/ru/tusur/udo/sensors/App.java
@@ -1,13 +1,27 @@
 package ru.tusur.udo.sensors;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.camel.impl.DefaultCamelContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationConfig.xml");
+		List<Sensor> ls = (ArrayList<Sensor>) ctx.getBean("sensors");		
+//		try {
+//			camelCtx.start();
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		System.out.println("Sensors Start.");
+//		while(true){}
+	}
 }
